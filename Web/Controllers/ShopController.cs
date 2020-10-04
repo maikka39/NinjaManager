b18fd4e8096b96cb56771676987c358e4d0b00bf
Maik de Kruif <maik@maik.dev>
2020-10-04 19:01:56 +0200

    Add edit modal

--- Web/Controllers/ShopController.cs
+++ Web/Controllers/ShopController.cs
@@ -6,11 +6,17 @@ namespace Web.Controllers
     public class ShopController : Controller
     {
         private readonly IEquipmentRepository _repo = new EquipmentRepository();
-        
+
         public IActionResult Index()
         {
             var equipments = _repo.GetAll();
-            
+
+            return View(equipments);
+        }
+        public IActionResult Edit()
+        {
+            var equipments = _repo.GetAll();
+
             return View(equipments);
         }
     }
