7a25860537c169e65df6c81f75b583c23138d97e
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2020-10-23 02:30:23 +0200

    Add Edit View
    Implemented the edit view for the Ninjas, you can now change name, gold and skinurl.

--- Web/Controllers/NinjaController.cs
+++ Web/Controllers/NinjaController.cs
@@ -20,6 +20,19 @@ namespace Web.Controllers
             return View(new Ninja());
         }
         
+        public IActionResult Details()
+        {
+            // Needs to be changed
+            var model = _repo.GetAll();
+            return View(model);
+        }
+        
+        public IActionResult Delete()
+        {
+            // Needs to be changed
+            return null;
+        }
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create([Bind("Name,Gold")] Ninja ninja)
@@ -33,6 +46,26 @@ namespace Web.Controllers
             if (!ModelState.IsValid) return View(ninja);
             
             _repo.Create(ninja);
+            
+            
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult Edit(int id)
+        {
+            if (id == 0) return RedirectToAction("Index");
+            
+            var ninja = _repo.GetOne(id);
+            return View(ninja);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([Bind("Id,Name,Gold,SkinUrl")] Ninja ninja)
+        {
+            if (!ModelState.IsValid) return View(ninja);
+            
+            _repo.Update(ninja);
             
             
             return RedirectToAction("Index");
