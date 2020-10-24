7e80f347907dfa2fe3a37458d9e28714d89a274c
Maik de Kruif <maik@maik.dev>
2020-10-25 00:30:40 +0200

    Fix ninja create

--- Web/Controllers/NinjaController.cs
+++ Web/Controllers/NinjaController.cs
@@ -14,53 +14,46 @@ namespace Web.Controllers
 
             return View(model);
         }
-        
+
         public IActionResult Create()
         {
             return View(new Ninja());
         }
-        
+
         public IActionResult Delete()
         {
             // Needs to be changed
             return null;
         }
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create([Bind("Name,Gold")] Ninja ninja)
+        public IActionResult Create([Bind("Name,Gold,SkinUrl")] Ninja ninja)
         {
-            foreach (var v in ModelState.Keys)
-            {
-                Console.WriteLine(v);
-                Console.WriteLine(ModelState[v].AttemptedValue);
-            }
-
             if (!ModelState.IsValid) return View(ninja);
-            
+
             _repo.Create(ninja);
-            
-            
+
             return RedirectToAction("Index");
         }
-        
+
         public IActionResult Edit(int id)
         {
             if (id == 0) return RedirectToAction("Index");
-            
+
             var ninja = _repo.GetOne(id);
             return View(ninja);
         }
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit([Bind("Id,Name,Gold,SkinUrl")] Ninja ninja)
         {
             if (!ModelState.IsValid) return View(ninja);
-            
+
             _repo.Update(ninja);
-            
-            
+
+
             return RedirectToAction("Index");
         }
     }
