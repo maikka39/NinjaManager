bf6b92e9d520c39332219f41e62c068011f01c10
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2020-10-25 23:15:41 +0100

    Update Details

---
+++ Web/wwwroot/js/ninja_details.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function(){
+    // Cache the highest
+    let highestBox = 0;
+    let cards = $('.fixed-card-size');
+    
+    // Select and loop the elements you want to equalise
+    cards.each(function(){
+        // If this box is higher than the cached highest then store it
+        if($(this).height() > highestBox) {
+            highestBox = $(this).height();
+        }
+    });
+    
+    // Select and loop the container element of the elements you want to equalise
+    cards.each(function(){
+
+        // Set the height of all those children to whichever was highest 
+        $('.fixed-card-size').height(highestBox);
+
+    });
+});
