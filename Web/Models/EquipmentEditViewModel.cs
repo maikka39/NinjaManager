6dbce9531a2621cdddfbe0e3184203a23f7bc46c
Maik de Kruif <maik@maik.dev>
2020-10-25 19:00:45 +0100

    Add viewmodels for edit

---
+++ Web/Models/EquipmentEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Data;
+
+namespace Web.Models
+{
+    public class EquipmentEditViewModel : EquipmentCreateViewModel
+    {
+        public int Id { get; set; }
+
+        public EquipmentEditViewModel(Equipment equipment) : base(equipment)
+        {
+            Id = equipment.Id;
+        }
+    }
+}
---
+++ Web/Models/EquipmentIndexViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Data;
+
+namespace Web.Models
+{
+    public class EquipmentIndexViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int Cost { get; set; }
+        public Category Category { get; set; }
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+        public int Agility { get; set; }
+
+        public EquipmentIndexViewModel(Equipment equipment)
+        {
+            Id = equipment.Id;
+            Name = equipment.Name;
+            Description = equipment.Description;
+            Cost = equipment.Cost;
+            ImageUrl = equipment.ImageUrl;
+            Category = equipment.Category;
+            Strength = equipment.Strength;
+            Intelligence = equipment.Intelligence;
+            Agility = equipment.Agility;
+        }
+    }
+}
